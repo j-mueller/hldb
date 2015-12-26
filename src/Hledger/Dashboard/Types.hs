25b573233d536ff6f15cc8c2eb60e283ae28d2e2
Jann Mueller <j.mueller.11@ucl.ac.uk>
2015-12-26 12:12:24 +0100

    Implement `accountsFor` and `balance`

--- src/Hledger/Dashboard/Types.hs
+++ src/Hledger/Dashboard/Types.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Hledger.Dashboard.Types where
 
-import           Control.Lens
+import           Control.Lens hiding ((...), singular)
 import           Data.AdditiveGroup
+import           Data.Foldable
 import           Data.Monoid
 import qualified Data.Map.Strict as M
 import           Data.Time.Calendar
+import           Numeric.Interval
 
 newtype Accounts = Accounts { _values :: M.Map String Rational }
   deriving (Eq, Ord)
@@ -22,28 +24,57 @@ instance AdditiveGroup Accounts where
   negateV = Accounts . fmap negate . view values
 
 data ReportingInterval = Day | Week | Month | Quarter | Year
-  deriving (Eq, Ord, Show, Enum)
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+enumerate :: (Enum a, Bounded a) => [a]
+enumerate = enumFromTo minBound maxBound
 
 -- | Journal contains accounts for various reporting periods
-newtype Journal = Journal {
-  _intervals :: M.Map (Day, ReportingInterval) Accounts
+data Journal = Journal {
+  _intervals :: M.Map (Day, ReportingInterval) Accounts,
+  _firstDay :: Maybe Day
 }
 
 makeLenses ''Journal
-
--- | Get the first day for which data is available in the journal
-startingDate :: Journal -> Maybe Day
-startingDate = undefined
 
 -- | Get the first day of the interval containing the given day
 -- | If the interval is `Day` then `begin` and `end` both evaluate to `id`
 begin :: ReportingInterval -> Day -> Day
 begin i = case i of
   Day -> id
-  _ -> undefined
+  Week -> undefined
 
 -- | Get the last day of the interval containing the given day
+-- | If the interval is `Day` then `begin` and `end` both evaluate to `id`
 end :: ReportingInterval -> Day -> Day
 end i = case i of
   Day -> id
   _ -> undefined
+
+-- | Get all intervals starting at a given date
+startingAt :: Day -> [ReportingInterval]
+startingAt d = takeWhile ((==) d . flip begin d) enumerate
+
+breakDown :: Interval Day -> [(Day, ReportingInterval)]
+breakDown i = current : rest where
+  f = inf i
+  t = sup i
+  nextStart = succ $ uncurry (flip end) current
+  current = (f, maximum $ filter endsBefore $ startingAt f)
+  endsBefore rpi = (end rpi f) <= t
+  rest = case singular i of
+    True  -> []
+    False -> breakDown $ nextStart ... t
+
+-- | Get accounts for an interval
+accountsFor :: Interval Day -> Journal -> Accounts
+accountsFor i j = fold accts where
+  start = maybe fd (min fd) $ view firstDay j
+  fd = inf i
+  lookp t = M.findWithDefault mempty t $ view intervals j
+  accts = map lookp $ breakDown i
+
+-- | Get balance since beginning of journal
+balance :: Day -> Journal -> Accounts
+balance d j = accountsFor (start ... d) j where
+  start = maybe d id $ view firstDay j
