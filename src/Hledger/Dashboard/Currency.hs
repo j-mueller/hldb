682289ea9698835bc5c6155a947574797c7327c6
Jann Mueller <j.mueller.11@ucl.ac.uk>
2015-12-27 16:36:22 +0100

    Add doctests for Currency

--- src/Hledger/Dashboard/Currency.hs
+++ src/Hledger/Dashboard/Currency.hs
@@ -1,25 +1,75 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Hledger.Dashboard.Currency where
+module Hledger.Dashboard.Currency(
+  Currency,
+  currency,
+  add
+) where
 
 import           Control.Lens hiding ((...), singular)
 import           Data.AdditiveGroup
 import           Data.List (intercalate)
 import           Data.Monoid
 import qualified Data.Map.Strict as M
+import           Data.Ratio
 
+-- | Values with currencies.
 newtype Currency = Currency { _values :: M.Map String Rational }
   deriving (Eq, Ord)
 
 makeLenses ''Currency
 
-instance Monoid Currency where
-  mempty = Currency M.empty
-  mappend l r = Currency $ M.unionWith (+) (l^.values) (r^.values)
+nonZero :: M.Map String Rational -> M.Map String Rational
+nonZero = M.filter (not . (==) 0)
 
+-- $setup
+-- >>> import Control.Applicative
+-- >>> import Test.QuickCheck
+-- >>> instance Arbitrary Currency where arbitrary = Currency <$> fmap M.fromList arbitrary
+-- >>> :set -XScopedTypeVariables
+
+-- | `Currency` is a `Monoid` under addition and empty set of amounts.
+--
+-- The following properties hold:
+--
+-- prop> \(r :: Currency) -> r <> mempty == r
+-- prop> \(r :: Currency) -> mempty <> r == r
+-- prop> \((a, b, c) :: (Currency, Currency, Currency)) -> (a <> b) <> c == a <> (b <> c)
+instance Monoid Currency where
+  -- | Currency without any values
+  mempty = Currency M.empty
+
+  -- | Combine two `Currency`s by adding their values
+  mappend l r = Currency $ nonZero $ M.unionWith (+) (l^.values) (r^.values)
+
+-- | `Currency` is an additive group where `zeroV` and `^+^` are the monoid op-
+--   erations and `negateV` is negating the amounts
+--
+-- The following properties hold:
+--
+-- prop> \(r :: Currency) -> r ^+^ (negateV r) == zeroV
+-- prop> \((l, r) :: (Currency, Currency)) -> l ^+^ r == r ^+^ l
 instance AdditiveGroup Currency where
   zeroV = mempty
   l ^+^ r = l <> r
   negateV = Currency . fmap negate . view values
 
 instance Show Currency where
-  show c = intercalate " " $ fmap (\(k, v) -> show v ++ " " ++ k) $ M.assocs $ view values c
+  show c = intercalate ", " $ fmap (\(k, v) -> (showF v) ++ " " ++ k) $ M.assocs $ view values c where
+    showF :: Rational -> String
+    showF v' = show ((a / b) :: Double) where (a, b) = (fromInteger $ numerator v', fromInteger $ denominator v')
+
+-- | Create a `Currency` with a single value
+--
+-- >>> currency 1 "EUR"
+-- 1.0 EUR
+currency :: Rational -> String -> Currency
+currency r s = Currency $ M.fromList [(s, r)]
+
+-- | Add an amount to a `Currency`
+--
+-- >>> add 1 "EUR" $ currency 1 "EUR"
+-- 2.0 EUR
+-- >>> add 1 "GBP" $ currency 1 "EUR"
+-- 1.0 EUR, 1.0 GBP
+add :: Rational -> String -> Currency -> Currency
+add r s = Currency . M.insertWith (+) s r . view values
